bb311cb6f87de5451712205fd18b1fe610ba72de
HOT-WHEEL-APK <biswajeetpattnayak1155@gmail.com>
2026-09-20 18:48:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/hotwheel/selfdrive/MainActivity.java
+++ app/src/main/java/com/hotwheel/selfdrive/MainActivity.java
@@ -11,24 +11,56 @@ import android.content.Intent;
 import android.net.Uri;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         WebView web = new WebView(this);
         web.setBackgroundColor(Color.BLACK);
-        web.setWebViewClient(new WebViewClient());
-        WebSettings s = web.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(true);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        web.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                openExternal(url);
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, android.webkit.WebResourceRequest request) {
+                openExternal(request.getUrl().toString());
+                return true;
+            }
+        });
 
         web.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
-            try { startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url))); } catch (Exception ignored) {}
+            openExternal(url);
         });
 
         setContentView(web);
+
         web.loadUrl("file:///android_asset/index.html");
+    }
+
+    private void openExternal(String url) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        } catch (Exception e) {
+            try {
+                Intent browser = new Intent(Intent.ACTION_VIEW);
+                browser.setData(Uri.parse(url));
+                startActivity(browser);
+            } catch (Exception ignored) {
+            }
+        }
     }
 }
